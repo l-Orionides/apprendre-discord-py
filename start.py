00ac242412b3b15e5ef8de33920df4e6b749fdfb
l-Orionides <maxence.lair34@gmail.com>
2025-04-13 11:10:35 +0200

    Update start.py

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from keep_alive import keep_alive
+from commandes_slash.py import multiplication
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -15,5 +16,6 @@ class MonBot(commands.Bot):
 intents = discord.Intents.all()
 bot = MonBot(command_prefix='/', intents=intents)
 
+multiplication()
 keep_alive()
 bot.run(token=token)
