347a5fda0239a061832b66f626daf569200e6ff9
l-Orionides <maxence.lair34@gmail.com>
2025-04-13 11:12:19 +0200

    Update start.py

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from keep_alive import keep_alive
-from commandes_slash.py import multiplication
+from commandes_slash import multiplication
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
